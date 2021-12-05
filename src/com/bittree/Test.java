6474db69d5b2d5091deee34076853ddaca9f581b
ssm <785265379@qq.com>
2021-12-05 21:18:54 +0800

    ssm modify

--- src/com/bittree/Test.java
+++ src/com/bittree/Test.java
@@ -5,10 +5,7 @@ import java.util.Stack;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
-
-        System.out.println("hhhh");
-        System.out.println("hhhh");
-        System.out.println("hhhh");
+        System.out.println("no_hello");
+ 
     }
 }
