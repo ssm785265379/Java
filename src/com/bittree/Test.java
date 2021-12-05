46dc5259ee8874aa385c5ff18edfd8d632e3eee4
Bjy-bjy <834823562@qq.com>
2021-12-05 21:11:06 +0800

    bjysss

--- src/com/bittree/Test.java
+++ src/com/bittree/Test.java
@@ -7,8 +7,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
 
-        System.out.println("hhhh");
-        System.out.println("hhhh");
-        System.out.println("hhhh");
     }
 }
